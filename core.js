18a8d5b8ca3c012449e5674a8ea9b791e30e8b32
Yitzhak Andrade <yitzhak.andrade@aec.com.br>
2018-01-24 15:04:48 -0200

    Commit inicial.

---
+++ core.js
@@ -0,0 +1,15 @@
+﻿var request = require('request-promise')
+
+module.exports = {
+    get: function(url) {
+        return request(url)
+    },
+    mountQueryStr: function (query) {
+        var parameters = []
+        for (attr in query)
+            parameters.push(attr + '=' + query[attr])
+        if (parameters.length == 0) return ''
+        var queryStr = '?' + parameters.join('&')
+        return queryStr
+    }
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+﻿var core = require('./core')
+
+function createRoute(url, routeName) {
+    return {
+        get: function (id, subroute, query) {
+            var queryStr = core.mountQueryStr(query)
+            var urlArr = [ routeName, id, subroute ]
+            urlArr.forEach(function(e) {
+                if (e) url += '/' + e
+            })
+            url += queryStr
+            return core.get(url)
+        }
+    }
+}
+
+function createRoutes(url, routesNames) {
+    var routes = {}
+    routesNames.forEach(function(val, ix, arr) {
+        routes[val] = createRoute(url, val)
+    })
+    return routes
+}
+
+module.exports = {
+    createRoutes: createRoutes,
+    createRoute: createRoute
+}
